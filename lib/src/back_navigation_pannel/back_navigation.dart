6e9fbe61b6267b4849b96fcab0beea4a865ba95a
rscripnic <70273985+rscripnic@users.noreply.github.com>
2024-09-28 21:43:32 +0300

    Add backNavigation widget (#10)
    Co-authored-by: devscripnic <devscripnic@gmail.com>

---
+++ lib/src/back_navigation_pannel/back_navigation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../fuelet_uikit.dart';
+import 'circle_back_button.dart';
+import 'back_navigation_size.dart';
+
+class BackNavigation extends StatelessWidget {
+  final String title;
+  final Widget? suffixTitleWidget;
+  final Widget? action;
+  final bool disableBackBtn;
+  const BackNavigation(
+      {super.key,
+      required this.title,
+      this.suffixTitleWidget,
+      this.action,
+      this.disableBackBtn = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+            width: BackNavigationSize.backNavigationButtonSize,
+            height: BackNavigationSize.backNavigationButtonSize,
+            child:
+                !disableBackBtn ? const CircleBackButton() : const SizedBox()),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              suffixTitleWidget != null
+                  ? Row(
+                      children: [
+                        SizedBox(
+                            width: BackNavigationSize
+                                .backNavigationSuffixTittleSize,
+                            height: BackNavigationSize
+                                .backNavigationSuffixTittleSize,
+                            child: suffixTitleWidget!),
+                        const SizedBox(
+                          width: 6,
+                        )
+                      ],
+                    )
+                  : const SizedBox(),
+              Flexible(
+                child: Text(
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  title,
+                  style: NFTTypography.header5SemiBold.copyWith(
+                    color: NFTTheme.of(context).colorScheme.mainTextColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        SizedBox(
+          width: BackNavigationSize.backNavigationActionSize,
+          height: BackNavigationSize.backNavigationActionSize,
+          child: action ?? const SizedBox(),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/src/back_navigation_pannel/back_navigation_size.dart
@@ -0,0 +1,5 @@
+class BackNavigationSize {
+  static const double backNavigationButtonSize = 40;
+  static const double backNavigationSuffixTittleSize = 32;
+  static const double backNavigationActionSize = 26;
+}
