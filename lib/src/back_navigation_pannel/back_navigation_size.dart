45f35cad9a2d5490458f13faeeefe7bd1c6b8107
devscripnic <devscripnic@gmail.com>
2024-10-01 15:35:58 +0300

    changed chart tooltipTitleColor and backNavigationActionSize

--- lib/src/back_navigation_pannel/back_navigation_size.dart
+++ lib/src/back_navigation_pannel/back_navigation_size.dart
@@ -1,5 +1,5 @@
 class BackNavigationSize {
   static const double backNavigationButtonSize = 40;
   static const double backNavigationSuffixTittleSize = 32;
-  static const double backNavigationActionSize = 26;
+  static const double backNavigationActionSize = 40;
 }
